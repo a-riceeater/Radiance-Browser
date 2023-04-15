b6eab9c56fb994a56eed95abe1dab68d6889bd17
darthvader1925 <elijb12@gmail.com>
2023-04-15 01:31:24 -0700

    add searching functions

--- web/page.js
+++ web/page.js
@@ -1,3 +1,7 @@
+const remote = require("@electron/remote")
+const r = /(https?:\/\/(?:www\.|(?!www))[a-zA-Z0-9][a-zA-Z0-9-]+[a-zA-Z0-9]\.[^\s]{2,}|www\.[a-zA-Z0-9][a-zA-Z0-9-]+[a-zA-Z0-9]\.[^\s]{2,}|https?:\/\/(?:www\.|(?!www))[a-zA-Z0-9]+\.[^\s]{2,}|www\.[a-zA-Z0-9]+\.[^\s]{2,})/gi;
+const urlRegex = new RegExp(r);
+
 document.getElementById("navigatorSearch").addEventListener("keydown", (e) => {
     const search = document.getElementById("navigatorSearch");
     const placeholder = document.getElementById("navsPlaceholder")
@@ -13,5 +17,14 @@ document.getElementById("navigatorSearch").addEventListener("keydown", (e) => {
                 search.appendChild(phe)
             }
         })
+    }
+
+    if (e.key == "Enter") {
+        const win = remote.getCurrentWindow();
+        if (urlRegex.test(search.innerText)) {
+            win.loadURL(search.innerText)
+        } else {
+            win.loadURL(`https://google.com/search?q=${search.innerText}`)
+        }
     }
 })
